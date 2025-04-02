e5f2ee95c0b6273743e77059c82bc52377499432
Kashif <admin@kashifali.me>
2025-04-02 17:34:52 +0500

    chore(routes): group admin routes and apply route naming conventions

--- routes/web.php
+++ routes/web.php
@@ -5,18 +5,28 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\AdminController;
 
 Route::get('/', [ProductController::class, 'index']);
-
 Route::get('/products/{product_id}', [ProductController::class, 'show'])->name('products.show');
 
-Route::get('/login', [AdminController::class, 'loginPage'])->name('login');
-Route::post('/login', [AdminController::class, 'login'])->name('login.submit');
+Route::name('login')
+    ->controller(AdminController::class)
+    ->group(function () {
+        Route::get('/login', 'loginPage');
+        Route::post('/login', 'login')->name('.submit');
+    });
 
 Route::middleware(['auth'])->group(function () {
-    Route::get('/admin/products', [AdminController::class, 'products'])->name('admin.products');
-    Route::get('/admin/products/add', [AdminController::class, 'addProductForm'])->name('admin.add.product');
-    Route::post('/admin/products/add', [AdminController::class, 'addProduct'])->name('admin.add.product.submit');
-    Route::get('/admin/products/edit/{id}', [AdminController::class, 'editProduct'])->name('admin.edit.product');
-    Route::post('/admin/products/edit/{id}', [AdminController::class, 'updateProduct'])->name('admin.update.product');
-    Route::get('/admin/products/delete/{id}', [AdminController::class, 'deleteProduct'])->name('admin.delete.product');
     Route::get('/logout', [AdminController::class, 'logout'])->name('logout');
 });
+
+Route::prefix('admin')
+    ->name('admin.')
+    ->middleware(['auth'])
+    ->controller(AdminController::class)
+    ->group(function () {
+        Route::get('/products', 'products')->name('products');
+        Route::get('/products/add', 'addProductForm')->name('add.product');
+        Route::post('/products/add', 'addProduct')->name('add.product.submit');
+        Route::get('/products/edit/{id}', 'editProduct')->name('edit.product');
+        Route::post('/products/edit/{id}', 'updateProduct')->name('update.product');
+        Route::get('/products/delete/{id}', 'deleteProduct')->name('delete.product');
+    });
