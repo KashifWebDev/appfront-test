8359584398a340a586896196132dd15e61f2c6b8
Kashif <admin@kashifali.me>
2025-04-02 17:15:41 +0500

    refactor(controller): move product update logic to service layer

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,59 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\ExchangeRateService;
+use App\Services\ProductService;
 use Illuminate\Http\Request;
 use App\Models\Product;
+use Illuminate\View\View;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class ProductController extends Controller
 {
-    public function index()
+
+    public function __construct(
+        protected ProductService $productService,
+        protected ExchangeRateService $exchangeRateService){
+        //
+    }
+
+    public function index(): View
     {
-        $products = Product::all();
-        $exchangeRate = $this->getExchangeRate();
+        $products = $this->productService->getAllProducts();
+        $exchangeRate = $this->exchangeRateService->getUsdExchangeRate();
 
         return view('products.list', compact('products', 'exchangeRate'));
     }
 
-    public function show(Request $request)
+    public function show(Request $request): View
     {
-        $id = $request->route('product_id');
-        $product = Product::find($id);
-        $exchangeRate = $this->getExchangeRate();
+        $id = (int) $request->route('product_id');
+        $product = $this->productService->findProductById($id);
+
+        if (!$product) {
+            throw new NotFoundHttpException('Product not found');
+        }
+
+        $exchangeRate = $this->exchangeRateService->getUsdExchangeRate();
 
         return view('products.show', compact('product', 'exchangeRate'));
     }
 
-    /**
-     * @return float
-     */
-    private function getExchangeRate()
-    {
-        try {
-            $curl = curl_init();
-
-            curl_setopt_array($curl, [
-                CURLOPT_URL => "https://open.er-api.com/v6/latest/USD",
-                CURLOPT_RETURNTRANSFER => true,
-                CURLOPT_TIMEOUT => 5,
-                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                CURLOPT_CUSTOMREQUEST => "GET",
-            ]);
-
-            $response = curl_exec($curl);
-            $err = curl_error($curl);
-
-            curl_close($curl);
-
-            if (!$err) {
-                $data = json_decode($response, true);
-                if (isset($data['rates']['EUR'])) {
-                    return $data['rates']['EUR'];
-                }
-            }
-        } catch (\Exception $e) {
-
-        }
-
-        return env('EXCHANGE_RATE', 0.85);
-    }
 }
